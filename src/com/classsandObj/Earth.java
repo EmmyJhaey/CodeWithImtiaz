f1dc5c1826e3b8e8c92f3013cd5aef941f8dc9e3
JavaDev <emmyj1192@gmail.com>
2023-02-27 11:02:38 -0600

    obj and classes

---
+++ src/com/classsandObj/Earth.java
@@ -0,0 +1,14 @@
+package com.classsandObj;
+
+public class Earth {
+
+    public static void main(String[] args) {
+            Human.setName("Tommy");
+            Human.setGender("M");
+            Human.setHeight(23F);
+            Human.setWeight(60F);
+
+            Human.walk();
+    }
+
+}
---
+++ src/com/classsandObj/Human.java
@@ -0,0 +1,67 @@
+package com.classsandObj;
+//Blueprint of how a human should be created
+public class Human {
+        //Physical Attributes of a Human
+        static String  gender;
+        static String hairColor;
+        static String name;
+        static float height;
+        static float weight;
+
+//        public  Human(String gender, String hairColor, String name, float height, float weight){
+//            this.gender = gender;
+//            this.hairColor = hairColor;
+//            this.name = name;
+//            this.height = height;
+//            this.weight  = weight;
+//        }
+
+        public static void  setName(String name){
+            Human.name = name;
+        }
+
+        public static String getName(){
+            return name;
+        }
+
+    public static String getGender() {
+        return gender;
+    }
+
+    public static void setGender(String gender) {
+        Human.gender = gender;
+    }
+
+    public static String getHairColor() {
+        return hairColor;
+    }
+
+    public static void setHairColor(String hairColor) {
+        Human.hairColor = hairColor;
+    }
+
+    public static float getHeight() {
+        return height;
+    }
+
+    public static void setHeight(float height) {
+        Human.height = height;
+    }
+
+    public static float getWeight() {
+        return weight;
+    }
+
+    public static void setWeight(float weight) {
+        Human.weight = weight;
+    }
+
+    public static void eat(){
+            System.out.println(name + " is eating");
+        }
+
+        public  static void walk(){
+            System.out.println(name + " is walking");
+        }
+
+}
